d64d97c3eab1ab6550f5096ee598810db8b6433e
Deepak <deepakkumar750206@gmail.com>
2022-11-26 16:55:07 +0530

    String .equals() function understanding

---
+++ String/Compare.java
@@ -0,0 +1,39 @@
+package String;
+
+public class Compare {
+    public static void main(String[] args){
+        String str = "hello"; // String literal
+        String str2 = "hello"; // String literal
+        String str3 = new String("hello"); // String object
+
+        //here str1 & str2 share storage in common poool
+        // i.e. same reference
+        if(str  == str2){
+            System.out.println("Both are equals");
+        } else {
+            System.out.println("Both are not equals");
+        }
+
+      
+        //here str1 & str3 share storage in different poool
+        // i.e. different reference
+        if(str  == str3){
+            System.out.println("Both are equals");
+        } else {
+            System.out.println("Both are not equals");
+        }
+
+        // == compare with reference not with value
+
+
+        if(str.equals(str3)){
+            System.out.println("Both are equals");
+        } else {
+            System.out.println("Both are not equals");
+        }
+
+        //.equals() compare with value
+
+
+    }
+}
