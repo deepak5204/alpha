df57def609dbb8b88fdcd2b7f8899f85adcb5082
Deepak <deepakkumar750206@gmail.com>
2022-11-26 18:31:09 +0530

    understand compareTo method

--- String/PrintLargestString.java
+++ String/PrintLargestString.java
@@ -1,7 +1,23 @@
+
+//Time complexity  O(x*N)
+//here x is length of string
+
 package String;
 
 public class PrintLargestString {
+
     public static void main(String[] args){
-        String[] str = {"apple", "mango", "banana"};
+        String[] fruits = {"apple", "mango", "banana"};
+
+        String largest = fruits[0];
+
+        for(int i = 1; i < fruits.length; i++){
+            if(largest.compareToIgnoreCase(fruits[i]) < 0){
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
     }
 }
+
+
