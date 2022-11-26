5843260a736423e3cf77b0808bc2a56030fac643
Deepak <deepakkumar750206@gmail.com>
2022-11-26 18:35:36 +0530

    Prit triplet whose sum = 0

--- Array/Triplet.java
+++ Array/Triplet.java
@@ -16,7 +16,7 @@ public class Triplet {
 
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
         int n = nums.length;
-        
+        //brute force approach
         for(int i = 0; i < n-2; i++){
             for(int j = i+1; j < n-1; j++){
                 for(int k = j+1; k < n; k++){
