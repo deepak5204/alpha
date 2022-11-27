f9000d850c9e4bc5a9920657388fa991bb5964a5
Deepak <deepakkumar750206@gmail.com>
2022-11-27 15:36:07 +0530

    check vowels in a string

---
+++ String/CheckLowerCaseVowel.java
@@ -0,0 +1,26 @@
+package String;
+
+import java.util.Scanner;
+
+public class CheckLowerCaseVowel {
+
+    public static int checkVowels(String str) {
+        int count = 0;
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' ||str.charAt(i) == 'u'){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        sc.close();
+
+        int res = checkVowels(str);
+        System.out.println(res);
+    }
+}
