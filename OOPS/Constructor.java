ae4b7e311eb35657e5163f4d5e7f5059c72b67ff
Deepak <deepakkumar750206@gmail.com>
2022-11-30 15:02:58 +0530

    types of constructors

--- OOPS/Constructor.java
+++ OOPS/Constructor.java
@@ -1,8 +1,24 @@
 package OOPS;
 
+
+
+/*
+    default constructor call when other constructor is not defined
+
+ */
+
+ 
 public class Constructor {
     public static void main(String[] args) {
-        Student s1 = new Student();
+        Student s1 = new Student(); // call Non-parameterized constructor
+        System.out.println(s1.name);
+        System.out.println(s1.rollno);
+        Student s2 = new Student("deepak"); // call Parameterized constructor
+        System.out.println(s2.name);
+        System.out.println(s2.rollno);
+        Student s3 = new Student(124);
+        System.out.println(s3.name);
+        System.out.println(s3.rollno);
     }
     
 }
@@ -12,7 +28,17 @@ class Student{
     int rollno;
 
     // create constructor
-    Student(){
+    Student(){ // itis non - parametrized constructor
         System.out.println("constructor is called");
     }
+
+    // create parametrized constructor
+    Student(String name){
+        this.name = name;
+    }
+
+        // create parametrized constructor
+        Student(int rollno){
+            this.rollno = rollno;
+        }
 }
