9ff377a77127cd8b0e9fa3a261fbd8c3a1b5ec7e
Deepak <deepakkumar750206@gmail.com>
2022-12-10 09:07:43 +0530

    print binary string which not contain consecutive 1's

---
+++ Recursion/PrintBinaryString.java
@@ -0,0 +1,30 @@
+package Recursion;
+
+public class PrintBinaryString {
+
+    public static void binaryString(int n, int lastPlace, String str){
+        //base case
+        if(n == 0){
+            System.out.println(str);
+            return;
+        }
+
+        // // if lastPlace is 0
+        // if(lastPlace == 0){
+        //     binaryString(n - 1, 0, str+"0");
+        //     binaryString(n - 1, 1, str+"1");
+        // } else {
+        //     binaryString(n - 1, 0, str+"0");
+        // }
+
+        binaryString(n - 1, 0, str+"0");
+        if(lastPlace == 0){
+            binaryString(n - 1, 1, str+"1");
+        }
+
+    }
+    public static void main(String[] args){
+        binaryString(4, 0, "");
+
+    }
+}
