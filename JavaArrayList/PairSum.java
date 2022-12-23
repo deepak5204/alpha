4c222a0e105e2c03e416db1e14a2e4220a8b44e6
Deepak <deepakkumar750206@gmail.com>
2022-12-23 20:11:03 +0530

    pair sum using TWO POINTER APPROACH approach, here list is SORTED in ascending order

--- JavaArrayList/PairSum.java
+++ JavaArrayList/PairSum.java
@@ -4,15 +4,37 @@ import java.util.ArrayList;
 
 public class PairSum {
 
-    //Brute force approach
+    // //Brute force approach
+    // public static boolean isPairSumExist(ArrayList<Integer> list, int target){
+    //     for(int i = 0; i < list.size(); i++){
+    //         for(int j = i+1; j < list.size(); j++){
+    //             if(list.get(i) + list.get(j) == target){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // 2 pointer Approach
     public static boolean isPairSumExist(ArrayList<Integer> list, int target){
-        for(int i = 0; i < list.size(); i++){
-            for(int j = i+1; j < list.size(); j++){
-                if(list.get(i) + list.get(j) == target){
-                    return true;
-                }
-            }
+        int lp = 0;
+        int rp = list.size() - 1;
+        while(lp < rp){
+            //case 1
+            if(list.get(lp) + list.get(rp) == target){
+                return true;
+            } 
+            //case 2
+             else if(list.get(lp) + list.get(rp) < target){
+                lp++;
+             } 
+             //case 3
+             else{
+                rp--;
+             }
         }
+
         return false;
     }
     public static void main(String[] args) {
