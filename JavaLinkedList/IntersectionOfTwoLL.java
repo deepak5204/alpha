861376141ab65e1924466f1014ec79a774314bec
Deepak <deepakkumar750206@gmail.com>
2022-12-26 15:03:01 +0530

    find Intersection of two Linked list

---
+++ JavaLinkedList/IntersectionOfTwoLL.java
@@ -0,0 +1,57 @@
+package JavaLinkedList;
+
+public class IntersectionOfTwoLL {  // TC= O(m*n)
+                                    // sc= O(1)
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    //find intersection Point
+    public Node getIntersection(Node h1, Node h2){
+        while(h2 != null){
+            Node temp = h1;
+            while(temp != null){
+                if(temp == h2){
+                    return h2;
+                }
+                temp = temp.next;
+            }
+            h2 = h2.next;
+        }
+        return null;
+    }
+    public static void main(String[] args){
+        IntersectionOfTwoLL list = new IntersectionOfTwoLL();
+        Node head1, head2;
+        head1 = new Node(10);
+        head2 = new Node(3);
+        
+        Node newNode = new Node(6);
+        head2.next = newNode;
+
+        newNode = new Node(9);
+        head2.next.next = newNode;
+
+        newNode = new Node(15);
+        head1.next = newNode;
+        head2.next.next.next = newNode;
+        // System.out.println(head1.next.data);
+        // System.out.println(head2.next.next.next.data);
+        
+        newNode = new Node(30);
+        head1.next.next = newNode;
+
+        head1.next.next.next = null;
+
+        System.out.println(list.getIntersection(head1, head2).data);
+
+
+
+    }
+}
