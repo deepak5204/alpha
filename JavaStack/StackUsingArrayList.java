785bd4a51db73534fec1f1f83891315576752754
Deepak <deepakkumar750206@gmail.com>
2022-12-26 15:37:39 +0530

    implement stack methods using arraylist

---
+++ JavaStack/StackUsingArrayList.java
@@ -0,0 +1,53 @@
+package JavaStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.Popup;
+
+public class StackUsingArrayList {
+    public static class Stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+        
+        //isEmpty
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        //push
+        public static void push(int data){
+            list.add(data);
+        }
+
+        // peek
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return list.get(list.size() - 1);
+        }
+        //pop
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int val = peek();
+            list.remove(list.size() - 1);
+            return val;
+        }   
+    }
+
+
+    public static void main(String[] args){
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(! s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+        
+    }
+}
