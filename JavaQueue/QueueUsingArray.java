e6077b2aff234b5289700c24ba20fb6ddcdb8e8c
Deepak <deepakkumar750206@gmail.com>
2022-12-28 11:18:30 +0530

    Queue using Array

---
+++ JavaQueue/QueueUsingArray.java
@@ -0,0 +1,71 @@
+package JavaQueue;
+
+public class QueueUsingArray {
+    
+    static class Queue{
+        static int [] arr;
+        static int size;
+        static int rear;
+         
+        Queue(int n){
+            arr = new int[n];
+            size = n;
+            rear = -1;
+        }
+
+        //empty queue
+        public boolean isEmpty(){
+            return rear == -1;
+        }
+
+        //add
+        public void add(int data){
+            if(rear == size-1){
+                System.out.println("Queue is full");
+                return;
+            }
+            rear = rear + 1;
+            arr[rear] = data;
+        }
+
+        // remove
+        public int remove(){
+            if(isEmpty()){
+               System.out.println("Empty Queue"); 
+               return -1;
+            }
+            int front = arr[0];
+            for(int i = 0; i < rear; i++){
+                arr[i] = arr[i+1];
+            }
+            rear = rear - 1;
+            return front;
+        }
+
+        //peek()
+        public int peek(){
+            if(isEmpty()){
+                System.out.println("Empty Queue"); 
+                return -1;
+             }
+             return arr[0];
+        }
+    }
+
+
+
+    public static void main(String[] args){
+        int n = 5;// size of queue
+        Queue q = new Queue(n);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        //print queue
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+ 
+    }
+}
