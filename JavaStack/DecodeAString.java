64c41a9c73d25cdd4d2421bef7b1e043c96457d1
Deepak <deepakkumar750206@gmail.com>
2022-12-28 08:48:27 +0530

    Decode a string

---
+++ JavaStack/DecodeAString.java
@@ -0,0 +1,68 @@
+package JavaStack;
+
+import java.util.Stack;
+
+public class DecodeAString { // TC = O(n)
+                             // sC = O(n)
+
+    public static String  simplify(String str) {
+        Stack<Character> stringstack = new Stack<>();
+        Stack<Integer> integerstack = new Stack<>();
+
+        String temp = "", result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+            if (Character.isDigit(str.charAt(i))) {
+                while (Character.isDigit(str.charAt(i))) {
+                    count = count * 10 + str.charAt(i) - '0';
+                    i++;
+                }
+                i--;
+                integerstack.push(count);
+            }
+                 else if (str.charAt(i) == ']') {
+                temp = "";
+                count = 0;
+
+                if (!integerstack.isEmpty()) {
+                    count = integerstack.peek();
+                    integerstack.pop();
+                }
+
+                while (!stringstack.isEmpty() && stringstack.peek() != '[') {
+                    temp = stringstack.peek() + temp;
+                    stringstack.pop();
+                }
+                if (!stringstack.empty() && stringstack.peek() == '[')
+                    stringstack.pop();
+                for (int j = 0; j < count; j++)
+                    result = result + temp;
+                for (int j = 0; j < result.length(); j++)
+                    stringstack.push(result.charAt(j));
+                result = "";
+            } else if (str.charAt(i) == '[') {
+                if (Character.isDigit(str.charAt(i - 1)))
+                    stringstack.push(str.charAt(i));
+                else {
+                    stringstack.push(str.charAt(i));
+                    integerstack.push(1);
+                }
+            }
+            else
+            stringstack.push(str.charAt(i));
+        }
+        while (!stringstack.isEmpty()) {
+            result = stringstack.peek() + result;
+            stringstack.pop();
+        }
+        return result;
+    }
+    
+
+    public static void main(String[] args){
+        String str = "2[cv]";
+        String res = simplify(str);
+        System.out.println(res);
+    }
+}
