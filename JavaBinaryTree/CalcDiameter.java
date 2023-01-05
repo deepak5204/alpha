88f3e728b00a353b8bc00f68bc8ca5593dd7d18f
Deepak <deepakkumar750206@gmail.com>
2023-01-05 22:52:14 +0530

    Calculate diameter of a tree

---
+++ JavaBinaryTree/CalcDiameter.java
@@ -0,0 +1,69 @@
+package JavaBinaryTree;
+
+public class CalcDiameter {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    //calculate height
+    public static int height(Node root){
+        if(root == null){
+            return 0;
+        }
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+        return Math.max(lh, rh) + 1;
+    }
+
+
+    //calculate diameter   
+    public static int diameter(Node root){  // O(n^2)
+        if(root == null){
+            return 0;
+        }
+        int ld = diameter(root.left);
+        int lht = height(root.left);
+        int rd = diameter(root.right);
+        int rht = height(root.right);
+
+        int selfDiam = lht + rht + 1;
+
+        return Math.max(selfDiam, Math.max(ld, rd));
+    }
+
+    public static void main(String[] args){
+/*
+            1
+          /   \
+         2     3
+        / \    / \
+       4   5  6   7
+          /        \
+         8          9
+ */
+
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.left.right.left = new Node(8);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        root.right.right.right = new Node(9);
+        
+
+        int d = diameter(root);
+        System.out.println("Diameter = "+ d);
+    }
+}
