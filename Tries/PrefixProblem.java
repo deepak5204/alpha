4b9f906d7cf1e41d743245dd4f040f8f25833d51
Deepak <deepakkumar750206@gmail.com>
2023-01-21 16:49:20 +0530

    Find unique prefix

---
+++ Tries/PrefixProblem.java
@@ -0,0 +1,75 @@
+package Tries;
+
+public class PrefixProblem {
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+        int freq;
+
+        public Node(){
+            for(int i = 0; i < 26; i++){
+                children[i] = null;
+            }
+            freq = 1;
+        }
+    }
+
+    public static Node root = new Node();
+
+    //create tries
+    public static void insert(String word){
+        Node curr = root;
+        for(int level = 0; level < word.length(); level++){
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            } else {
+                curr.children[idx].freq++;
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    //search
+    // public static boolean search(String word){
+    //     Node curr = root;
+    //     for(int level = 1; level < word.length(); level++){
+    //         int idx = word.charAt(level)-'a';
+    //         if(curr.children[idx] == null){
+    //             return false;
+    //         }
+    //         curr = curr.children[idx];
+    //     }
+    //     return true;
+    // }
+
+    //find Uniqueprefix
+    public static void findPrefix(Node root, String ans){
+        if(root == null){
+            return;
+        }
+
+        if(root.freq == 1){
+            System.out.println(ans);
+            return;
+        }
+
+        for(int i = 0; i < root.children.length; i++){
+            if(root.children[i] != null){
+                findPrefix(root.children[i], ans+(char)(i+'a'));
+            }
+        }
+    }
+    public static void main(String[] args){
+        String[] words = {"zebra", "dog", "duck", "dove"};
+        for(int i = 0; i < words.length; i++){
+            insert(words[i]);
+        }
+
+        root.freq = -1;
+
+        findPrefix(root, "");
+
+    }
+}
