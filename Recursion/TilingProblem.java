7576c14263d31a74a6b4ffadfcefa75d848d633b
Deepak <deepakkumar750206@gmail.com>
2023-03-10 18:39:30 +0530

    Specify the question of Tiling problem

--- Recursion/TilingProblem.java
+++ Recursion/TilingProblem.java
@@ -1,3 +1,8 @@
+/* Given a "2 x n" board and tiles of size "2 x 1", count number of
+    ways to tile the given board using the 2 x 1 tiles.
+    (A tile can either be placed horizontally or vertically.)
+ */
+
 package Recursion;
 
 import java.util.Scanner;
