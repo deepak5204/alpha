51f38aaabeb0d8df72ae9546588c951ff9478e2f
Deepak <pdeepak1255@gmail.com>
2023-07-30 12:07:47 +0530

    Dijkstra Algorithm

--- Graph/DijkstraAlgorithm.java
+++ Graph/DijkstraAlgorithm.java
@@ -22,13 +22,13 @@ public class DijkstraAlgorithm {
         graph[0].add(new Edge(0, 1, 2));
         graph[0].add(new Edge(0, 2, 4));
 
-        graph[1].add(new Edge(1, 2, -4));
+        graph[1].add(new Edge(1, 2, 1));
 
         graph[2].add(new Edge(2, 3, 2));
 
         graph[3].add(new Edge(3, 4, 4));
 
-        graph[4].add(new Edge(4, 1, -1));
+        graph[4].add(new Edge(4, 1, 1));
     }
 
     static class Pair implements Comparable<Pair>{
