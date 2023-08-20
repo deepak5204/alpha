649fd7f4c5206e6276b4a029ea45f9f1c97d6599
Deepak <pdeepak1255@gmail.com>
2023-08-20 19:11:05 +0530

    Target Sum Subset: print boolean dp

--- DP/TargetSumSubset.java
+++ DP/TargetSumSubset.java
@@ -24,11 +24,21 @@ public class TargetSumSubset {
                 }
             }
         }
+        printDP(dp);
         return dp[n][target];
     } 
+
+    public static void printDP(boolean [][] dp){
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[0].length; j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args){
         int arr[] = {4, 2, 7, 1, 3};
-        int target = 20;
+        int target = 10;
 
         System.out.println(targetSum(arr, target));
     }
