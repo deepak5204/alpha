f511220fb024dc126cdde5d13532fcbdc0822e68
Deepak <pdeepak1255@gmail.com>
2023-08-31 16:51:32 +0530

    Matrix Chain Multiplication: (MCM)

---
+++ DP/MCM.java
@@ -0,0 +1,30 @@
+package DP;
+
+public class MCM {
+
+    public static int mcm(int[] arr, int i, int j){
+        // System.out.println(i+" , "+j);
+        if(i == j){
+            return 0;
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for(int k = i;  k <= j-1; k++){
+            int cost1 = mcm(arr, i, k);  //Ai.....Ak => arr[i-1] x arr[k]
+            int cost2 = mcm(arr, k + 1, j); //Ak.....Aj => arr[k] x arr[j]
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+            
+            int finalCost = cost1 + cost2 + cost3;
+
+            ans = Math.min(ans, finalCost);
+        }
+        return ans; // minimum cost
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 3};
+        int n = arr.length;
+        System.out.print(mcm(arr, 1, n-1));
+
+    }
+}
