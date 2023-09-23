6f53a2ad09eb979e6a2b94c7fc5dcc2d801ced86
Deepak <pdeepak1255@gmail.com>
2023-09-23 18:02:06 +0530

    add comments and question of trapping water

--- Array/TrappingRainwater.java
+++ Array/TrappingRainwater.java
@@ -1,7 +1,12 @@
+/*
+  Given a non-negative integers representing an elevation map where the width of each bar is 1, 
+  compute how much water it can trap after raining
+
+  height = {4, 2, 0, 6, 3, 2, 5}
+ */
+
 package Array;
-
 public class TrappingRainwater {
-
     public static void totalTrappedwater(int[] height) {
         int LMB[] = new int[height.length];
         int lmax = height[0];
@@ -14,14 +19,12 @@ public class TrappingRainwater {
                 LMB[i] = lmax;
             }
         }
-
         // // print Left Max Boundary
         // for (int i = 0; i < LMB.length; i++) {
-        //     System.out.print(LMB[i] + " ");
+        // System.out.print(LMB[i] + " ");
         // }
 
         System.out.println();
-
         int RMB[] = new int[height.length];
         int rmax = height[height.length - 1];
         RMB[RMB.length - 1] = rmax;
@@ -33,27 +36,26 @@ public class TrappingRainwater {
                 RMB[i] = rmax;
             }
         }
-
         // // print Right Max Boundary
         // for (int i = 0; i < LMB.length; i++) {
-        //     System.out.print(RMB[i] + " ");
+        // System.out.print(RMB[i] + " ");
         // }
 
-
-        //calculate total trapped watersum
+        // calculate total trapped watersum
+        // waterLabel = min(max of LMB, max of RMB )
+        //  trapped water = waterLabel - height
         int totalTrappedWater = 0;
         int waterLabel = 0;
-        for(int i = 0; i < height.length; i++){
+        for (int i = 0; i < height.length; i++) {
             waterLabel = LMB[i] < RMB[i] ? LMB[i] : RMB[i];
 
-            if(waterLabel - height[i] < 0){
+            if (waterLabel - height[i] < 0) {
                 totalTrappedWater += 0;
             } else {
                 totalTrappedWater += waterLabel - height[i];
             }
         }
-
-        System.out.println("Total trapped water : "+totalTrappedWater);
+        System.out.println("Total trapped water : " + totalTrappedWater);
     }
 
     public static void main(String[] args) {
