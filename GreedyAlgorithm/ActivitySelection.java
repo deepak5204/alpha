abfc135b42bc143a11c4e9dfe502b451ec058c7f
Deepak <pdeepak1255@gmail.com>
2024-06-11 07:41:24 +0530

    Write question of Activity selection question in greedy algorithm

--- GreedyAlgorithm/ActivitySelection.java
+++ GreedyAlgorithm/ActivitySelection.java
@@ -1,3 +1,12 @@
+/*
+ * You are given 'n' activity with their 'n' start and 'n' end time. Select the maximum number of activities that can be 
+ * performed by a single person, assuming that a person can only work on a single activity at a time.
+ *  Activities are sorted according to end time.
+ *      int[] start = {1, 3, 0, 5, 8, 5};
+        int[] end = {2, 4, 6, 7, 9, 9};
+ */
+
+
 package GreedyAlgorithm;
 import java. util.*;
 
@@ -22,36 +31,36 @@ public class ActivitySelection {
         //end time sorted basis
         int maxAct = 0;
         ArrayList<Integer> ans = new ArrayList<>(); 
-        
-        // 1st activity
-        maxAct = 1;
-        ans.add(activities[0][0]);
-        int lastEnd = activities[0][2];
-        for(int i = 1; i < end.length; i++){
-            if(activities[i][1] >= lastEnd){
-                //activity select
-                maxAct++;
-                ans.add(activities[i][0]);
-                lastEnd = activities[i][2];
-            }
-        }
-
-
-
-
-
-        // // 1st activity
+        //-------------------------- Approach 2--------------------------
+        // 1st activity 
         // maxAct = 1;
-        // ans.add(0);
-        // int lastEnd = end[0];
+        // ans.add(activities[0][0]);
+        // int lastEnd = activities[0][2];
         // for(int i = 1; i < end.length; i++){
-        //     if(start[i] >= lastEnd){
+        //     if(activities[i][1] >= lastEnd){
         //         //activity select
         //         maxAct++;
-        //         ans.add(i);
-        //         lastEnd = end[i];
+        //         ans.add(activities[i][0]);
+        //         lastEnd = activities[i][2];
         //     }
         // }
+
+
+
+
+        //----------------------------- Approach 1--------------------------
+        // 1st activity
+        maxAct = 1;
+        ans.add(0);
+        int lastEnd = end[0];
+        for(int i = 1; i < end.length; i++){
+            if(start[i] >= lastEnd){
+                //activity select
+                maxAct++;
+                ans.add(i);
+                lastEnd = end[i];
+            }
+        }
 
         System.out.println("max activities = "+maxAct);
         for(int i = 0; i < ans.size(); i++){
