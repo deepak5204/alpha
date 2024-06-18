7d089cbd7d5ac2387f52b693769f8994f07f748f
Deepak <pdeepak1255@gmail.com>
2024-06-18 16:06:17 +0530

    Add question in Valid parenthesis

--- JavaStack/ValidParenthesis.java
+++ JavaStack/ValidParenthesis.java
@@ -1,3 +1,13 @@
+/*
+ * Valid Parentheses:
+ *  Given a string s containing just the characters '(', ')', '{', '}', '[', ']', determine if the input string is vaild.
+ *  An input string is valid if:
+ *      1.  Open brackets must be closed by the same type of brackets.
+ *      2.  Open brackets must be closed in the correct order.
+ *      3.  Every close brackets has a corresponding open bracket of the same type.
+ */
+
+
 package JavaStack;
 import java.util.Stack;
 
