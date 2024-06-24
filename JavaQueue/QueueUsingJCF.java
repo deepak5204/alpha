c38b5f3c1a0cc09dff5ee36447c720c600e51403
Deepak <pdeepak1255@gmail.com>
2024-06-24 18:03:48 +0530

    Write something about Queue using JCF

--- JavaQueue/QueueUsingJCF.java
+++ JavaQueue/QueueUsingJCF.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class QueueUsingJCF {
     public static void main(String[] args){
         //here we cannot create Queue object because Queue is an interface
+        // In Java Queue is implemented by "Linked List" and "ArrayDeque"
         // Queue<Integer> q = new LinkedList<>();  //ArrayDeque
         Queue<Integer> q = new ArrayDeque<>();
 
@@ -18,3 +19,13 @@ public class QueueUsingJCF {
 
     }
 }
+
+/* 
+//  LinkedList
+>>  Underlying Structure: LinkedList is implemented as a doubly-linked list.   Each element (node) holds a reference to both the next and previous elements in the list.
+>>  Memory Overhead: Higher memory overhead due to the storage of multiple references (two per element: next and previous).
+
+//  ArrayDeque
+>>  Underlying Structure: ArrayDeque is implemented using a resizable array. It maintains a circular buffer to store elements.
+>>  Memory Overhead: Lower memory overhead compared to LinkedList as it only stores the elements and a few integer variables for managing the circular buffer.
+*/
