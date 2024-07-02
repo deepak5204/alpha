bd765fafd486f0911a11cb98fbf7f0f284a7e655
Deepak <pdeepak1255@gmail.com>
2024-07-02 13:21:21 +0530

    Write question in Max Length Chain of pairs

--- GreedyAlgorithm/MaxLengthChainOfPairs.java
+++ GreedyAlgorithm/MaxLengthChainOfPairs.java
@@ -1,3 +1,11 @@
+/*
+ * You are given n pairs of numbers. In every pair, the first number is always smaller than the second number. A pair (c, d) can come after pair (a,b) if b < c.
+ * Find the longest chain which can be formed from a given set of pairs.
+ * 
+ * Sample input: paris = (5, 25), (39, 60), (5, 28), (27, 40), (50, 90)
+ * Sample output: 3
+ */
+
 package GreedyAlgorithm;
 import java.util.*;
 
